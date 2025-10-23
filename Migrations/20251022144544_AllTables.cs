453791d2668a9ee10641015a1457c5b9385d0d10
Antslabeu <wojtas@antslab.eu>
2025-10-23 18:41:51 +0200

    Added migration for EFCore; Added product categories sorting by ABC; Fixed product categories save ability;

---
+++ Migrations/20251022144544_AllTables.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Magazynek.Migrations
+{
+    /// <inheritdoc />
+    public partial class AllTables : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "settingname",
+                table: "systemsettings",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "type",
+                table: "product",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "settingname",
+                table: "systemsettings");
+
+            migrationBuilder.DropColumn(
+                name: "type",
+                table: "product");
+        }
+    }
+}
